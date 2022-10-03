1ceee94fe488da46120d033bace9271930904e96
Alfred Gaillard <alfred.gaillard@smart-chain.fr>
2022-10-03 13:26:40 +0200

    Working checked BIP39 seed generation

--- index.js
+++ index.js
@@ -1,66 +1,74 @@
 const { program } = require('commander');
 const crypto = require('crypto');
 const hexToBinary = require('hex-to-binary');
+const BIP39 = require('bip39');
 
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// MENU PART
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+program
+  .description('A sample bitcoin wallet')
+  .option('-g, --generate-seed', 'This option will generate a bitcoin wallet seed')
+  //.option('-b, --beta <VALUE>', 'Specify a VALUE', 'Foo');
+
+
+program.parse();
+const options = program.opts();
+//console.log("options:", options)
+if (Object.keys(options).length == 0) console.log("You should put an option");
+else {
+    if (options.generateSeed) generateSeed();
+}
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BIP 39 SEED GENERATION PART
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
 
 async function generateSeed() {
-    crypto.randomBytes(16, treatRandomNumber);
+    crypto.randomBytes(16, seedFromRandomNumber);
 }
 
-function treatRandomNumber(_err, buffer) {
-    const randomNumberBase16 = buffer.toString("hex"); // base16 string type number
-    const randomNumberBase2 = hexToBinary(randomNumberBase16); // 128 bits binary string representing our entropy
-    const randomNumberHashBase16 = generateSha256(randomNumberBase2); // sha256 in hex of the randomNumberBase2 value
+function seedFromRandomNumber(_err, buffer) {
+    // String version of our random number (which is represneting our 128 bits entropy)
+    const randomNumberBase16 = buffer.toString("hex"); // base16
+    const randomNumberBase2 = hexToBinary(randomNumberBase16); // base2
+
+    // GENERATING SHA256 HASH OF OUR 128 BITS HEX ENTROPY
+    const randomNumberHashBase16 = generateSha256(buffer); // sha256 in hex of the randomNumberBase2 value
+
+    // CONVERTING HASH FROM BASE 16 TO BASE 2
     const randomNumberHashBase2 = hexToBinary(randomNumberHashBase16); // sha256 binary converted
-    const randomizedEntropy = randomNumberBase2 + randomNumberHashBase2.slice(0, 4);
-    console.log("randomNumberBase16 : ", randomNumberBase16);
-    console.log("randomNumberBase2 : ", randomNumberBase2);
-    console.log("randomNumberHashBase16 : ", randomNumberHashBase16);
-    console.log("random number binary version : ", randomNumberHashBase2);
-    console.log("slice : ", randomNumberHashBase2.slice(0, 4));
-    console.log("randomizedEntropy : ", randomizedEntropy);
-    
+
+    // ADDING THE FIRST FOUR BITS OF THE HASH TO OUR 128 BITS ENTROPY TO GET A NEW 132 BITS ENTROPY
+    const entropyChecksummizedBase2 = randomNumberBase2 + randomNumberHashBase2.slice(0, 4);
+    // const randomizedEntropyBase16 = BigInt(randomizedEntropyBase2, 2).toString(16); // Not useful, show our 132 bits entropy in hex/base16
+
+    const line = "------------------------------------------------------------";
+    console.log(line, "\nGenerated random number (base16) :", randomNumberBase16 , "\n"+line);
+    console.log("Rnd number hash (SHA256, base16) :", randomNumberHashBase16 , "\n"+line);
+    console.log("Rnd number hash (SHA256, base2) :", randomNumberHashBase2 , "\n"+line);
+    console.log("1st four bits of the hash (base2) :", randomNumberHashBase2.slice(0, 4) , "\n"+line);
+    console.log("128 bits base2 entropy + sha256 hash 4 first bits = 132 bits entropy :", entropyChecksummizedBase2 , "\n"+line);
+    console.log("\n\nSeed from BIP39 dict :", getStringSeedFromEntropy(entropyChecksummizedBase2));
 }
 
-
-
-const text = 'Un obscur message venant du le système S-K, votre majesté. Ses hab la planète Terre';
-/*
-async function digestMessage(message) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(message);
-  const hash = await crypto.subtle.digest('SHA-256', data);
-  return hash;
+function getStringSeedFromEntropy(bitsSeed) {
+    let seed = "";
+    for (let index = 0; index < 12; index++) {
+        const slice = bitsSeed.slice(index*11, index*11+11);
+        //console.log("slice:", slice);
+        const sliceBase10 = parseInt(slice, 2);
+        //console.log("sliceBase10:", sliceBase10);
+        seed += BIP39.wordlists.english[parseInt(bitsSeed.slice(index*11, index*11+11), 2)] + " ";
+    }
+    return seed.slice(0, seed.length - 1);
 }
-
-async function executeSHACode() {
-    const digestBuffer = await digestMessage(text);
-    const decoded = new TextDecoder().decode(digestBuffer)
-    console.log(digestBuffer.byteLength);
-}
-
-function hex2bin(hex){
-    return (parseInt(hex, 16).toString(2)).padStart(8, '0');
-}
-
-executeSHACode();
-
-*/
 
 function generateSha256(input) {
     return crypto.createHash("sha256").update(input).digest("hex");
 }
-
-generateSeed();
-
-/*
-program
-  .description('A simple Bitcoin wallet');
-  //.option('-a, --alpha', 'Alpha')
-  //.option('-b, --beta <VALUE>', 'Specify a VALUE', 'Foo');
-*/
-
-
-//await console.log(generateRandomNumber(16).toString("hex"));
-
-
